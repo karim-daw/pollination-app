e921b46e94a6944284343010a5568268e288a1a7
Karim Daw <karimdaw91@gmail.com>
2021-12-02 16:50:21 +0100

    adapt naming

--- folder_utils.py
+++ folder_utils.py
@@ -1,19 +1,18 @@
-
 import os
-
-# reference from https://djangocentral.com/check-if-a-directory-exists-if-not-create-it/
 
 def create_folder(folderName):
     '''Check if directory exists, if not, create it'''
+    
+    # reference from https://djangocentral.com/check-if-a-directory-exists-if-not-create-it/
 
     # You should change 'test' to your preferred folder.
-    MYDIR = folderName
-    CHECK_FOLDER = os.path.isdir(MYDIR)
+    myDir = folderName
+    checkFolder = os.path.isdir(myDir)
 
     # If folder doesn't exist, then create it.
-    if not CHECK_FOLDER:
-        os.makedirs(MYDIR)
-        print("created folder : ", MYDIR)
+    if not checkFolder:
+        os.makedirs(myDir)
+        print("created folder : ", myDir)
 
     else:
-        print(MYDIR, "folder already exists.")
+        print(myDir, "folder already exists.")
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import shoe_box
 
 # init shoe_box 
-sb = shoe_box.Shoebox( width=6.0, height=4.0, depth=10.0)
+sb = shoe_box.Shoebox(width=6.0, height=4.0, depth=10.0)
 
-sb.gridSize = 0.5
+"""sb.gridSize = 0.5
 sb.gridOffset = 0.75
 sb.wwr = 0.4
-
+"""
 # create room
 room = sb.createRoom()
 
