3d7c219c071af63fdf026619729d62f402faf113
Karim Daw <karimdaw91@gmail.com>
2021-12-12 23:26:51 +0100

    add folder for all results

--- src/get_results.py
+++ src/get_results.py
@@ -1,6 +1,7 @@
 
 from pollination_rest_api.pollination import PollinationClient, Payload
 from urllib.request import urlretrieve
+from utils import folder_utils
 
 
 if __name__ == '__main__':
@@ -9,13 +10,15 @@ if __name__ == '__main__':
     projectName = 'AnnualDaylight-Shoebox4'
     projectID = '20a54a4a-ec9d-4747-92f1-7bc3049c3b0d'
 
+    # get runs from project id and unpack as json
     res = client.get_runs(projectName,projectID)
-
     body = res.json()
 
+    # create folder and file name
+    folderName = "{0}_Results".format(projectName)
+    folder_utils.createFolder(folderName)
+
     for run in body['resources']:
-        print(run)
-        print("hello")
         run_id = run['id']
         # The selected recipe determines the outputs
         run_output_name = run['status']['outputs'][0]['name']
@@ -24,5 +27,8 @@ if __name__ == '__main__':
         res = client.get_run_output(projectName, run_id, run_output_name)
         url = res.json()
 
+        fileName = folderName +"/" + f'{run_id}-{run_output_name}.zip'
+
+
         # Save to a local file
-        urlretrieve(url, f'{run_id}-{run_output_name}.zip')
+        urlretrieve(url,fileName )
