6a183b83d4dce615eed23d77e0ef029ad61982ff
karimdaw1991 <karimdaw91@gmail.com>
2022-05-09 19:48:35 +0200

    restored correct version of call pollination hopefully

--- src/call_pollination.py
+++ src/call_pollination.py
@@ -14,6 +14,7 @@ from queenbee.io.inputs.job import JobPathArgument
 from urllib.request import urlretrieve
 from pollination_rest_api.pollination import PollinationClient, Payload
 from utils import folder_utils
+from ladybug.wea import Wea
 
 if __name__ == '__main__':
     # Create client
@@ -23,7 +24,7 @@ if __name__ == '__main__':
     project = Payload.Create(
         name='AnnualDaylight-Shoebox4',
         description='This is a testing the annual daylight recipe on pollination using a shoebox model',
-        public=False
+        public=True
     )
     res = client.create_project(project)
     print(res.json())
@@ -36,6 +37,11 @@ if __name__ == '__main__':
     )
     res = client.add_recipe_to_project(project.name, recipe_filter)
     print(res.json())
+
+    # use handler to convert epw to wea
+
+    #wea = Wea.from_epw_file('path_to_epw_file')
+    #wea_file = wea.write('path_to_wea_file')
 
     # Create artifacts for 3D models and weatherfile
     file_names = os.listdir("models/honeybee-json-files")
@@ -51,12 +57,10 @@ if __name__ == '__main__':
         source=project_wea
     )
 
-    # Create 'artifacts' i.e models and files as payloads to send to pollination
+    # Create 'artifacts' i.e models as payloads to send to pollination
     arguments = []
     for name in file_names:
         artifact = Payload.Artifact(key=name)
-
-        # checking response
         res = client.add_file_to_project(project.name, artifact)
         print(res)
 
@@ -68,10 +72,7 @@ if __name__ == '__main__':
         )
 
         # Wrap each argument in its own list to parameterize
-        #arguments.append([wea_argument])
         arguments.append([model_argument, wea_argument])
-        
-    print(arguments)
 
     # Construct source URL for recipe
     recipe_source_url = '/'.join(
@@ -122,7 +123,6 @@ if __name__ == '__main__':
     body = res.json()
 
     for run in body['resources']:
-        print(run)
         run_id = run['id']
         # The selected recipe determines the outputs
         run_output_name = run['status']['outputs'][0]['name']
